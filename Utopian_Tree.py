3be7434b27536ebfb07422c10cb07068677d985d
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2020-04-23 14:07:47 +0530

    Create Utopian_Tree.py

---
+++ Utopian_Tree.py
@@ -0,0 +1,20 @@
+L=0
+mydict={}
+for i in range(0,60):
+    if i<=2:
+        L=L+1
+        mydict[i]=L
+    elif i%2==0 and i>2:
+        L=L+1
+        mydict[i]=L
+    elif i%2!=0 and i>2:
+        L=L*2
+        mydict[i]=L
+    elif i==0:
+        L=L+1
+        mydict[i]=L
+x=int(input())
+for i in range(x):
+    y=int(input())
+    if mydict.__contains__(y):
+        print(mydict[y])
