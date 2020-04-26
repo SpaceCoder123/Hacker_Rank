f72425cd96b24bda7a00c8b7fda64a1943ca5536
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2020-04-26 12:35:15 +0530

    Create Breaking_the_records.py

---
+++ Breaking_the_records.py
@@ -0,0 +1,27 @@
+Highest_Scores={}
+Lowest_Scores={}
+x=int(input())
+Score=list(map(int,input().split()))
+
+Highest_Scores={}
+Lowest_Scores={}
+Lowest=0
+Highest=0
+for i in range(x):
+    if i==0:
+        Highest_Scores[i]=Score[i]
+        Lowest_Scores[i]=Score[i]
+        Highest=Score[i]
+        Lowest=Score[i]
+    elif i>0:
+        if Score[i]>Highest :           
+            Highest=Score[i]
+            Highest_Scores[i]=Score[i]
+        elif Score[i]<Lowest:
+            Lowest_Scores[i]=Score[i]
+            Lowest=Score[i]
+
+        
+
+
+print(len(Highest_Scores)-1,len((Lowest_Scores))-1)
