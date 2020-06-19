fc83f49e146498ee3bd1bebf801a692d56162a82
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2020-06-19 20:47:15 +0530

    Create Swap_Case.py

---
+++ Swap_Case.py
@@ -0,0 +1,19 @@
+def swap_case(s):
+    X=[]
+    for i in s:
+        if i.islower():
+            X.append(i.upper())
+        elif i.isupper():
+            X.append(i.lower())
+        else:
+            X.append(i)
+    return ''.join(X)
+
+    
+    
+    return
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
